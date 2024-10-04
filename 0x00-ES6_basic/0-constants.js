430a70974e8e6f26efb8830b1a64610e2431bbc4
Mahao05 <prudencemolefe93@gmail.com>
2024-10-04 18:21:58 +0000

    Constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,26 +1,9 @@
-export function taskFirst() {
-    const taskName = "Complete homework";
-    const dueDate = "2024-10-10";
-    const priority = "High";
-
-    console.log(`Task: ${taskName}`);
-    console.log(`Due Date: ${dueDate}`);
-    console.log(`Priority: ${priority}`);
+function taskFirst() {
+  const task = 'First task';
+  return task;
+}
+function taskNext() {
+  let task = 'Next task';
+  return task;
 }
 
-taskFirs(t);
-
-export function taskNext() {
-    let taskName = "Prepare presentation";
-    let dueDate = "2024-10-15";
-
-    console.log(`Task: ${taskName}`);
-    console.log(`Due Date: ${dueDate}`);
-
-    taskName = "Update presentation";
-    dueDate = "2024-10-20";
-
-    console.log(`Updated Task: ${taskName}`);
-    console.log(`Updated Due Date: ${dueDate}`);
-}
-taskNext();

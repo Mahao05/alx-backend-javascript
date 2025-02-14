b1496d1a9f4863afa12d29487644560320a67ca1
Mahao05 <134053789+Mahao05@users.noreply.github.com>
2025-02-14 18:40:11 +0200

    Updated 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const host = '127.0.0.1';
-const port = 1245;
+const PORT = 1245;
+const HOST_NAME = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,8 @@ const app = http.createServer((req, res) => {
   res.end('Hello ALX!');
 });
 
-app.listen(port, host, () => {});
+app.listen(PORT, HOST_NAME, () => {
+  console.log(`Server running at http://${HOST_NAME}:${PORT}`);
+});
 
 module.exports = app;
